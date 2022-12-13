35204fe0259b7cef0e8bb6c01c5d2d118b71cdad
digital <digital@iMac24-105.local>
2022-12-13 17:30:52 +0100

    fin

--- TpFinalSwiftUI/TpFinalSwiftUI/AlbumPhotosView.swift
+++ TpFinalSwiftUI/TpFinalSwiftUI/AlbumPhotosView.swift
@@ -12,19 +12,35 @@ struct AlbumPhotosView: View {
     @ObservedObject var photoDownloader = UserPhotosDownloader()
     @State var triPhotosArray = Photos()
     var body: some View {
-        Grid {
-            ForEach(self.photoDownloader.photos){ photo in
-                if(self.triPhotosArray.count == 3){
-                    GridRow{
-                        Text("UNe row")
-                        AsyncImage(url: URL(string: self.triPhotosArray[0].url))
-                        AsyncImage(url: URL(string: self.triPhotosArray[1].url))
-                        AsyncImage(url: URL(string: self.triPhotosArray[2].url))
+        ScrollView{
+            Grid {
+                if(self.photoDownloader.photos.count > 0){
+                    ForEach(0..<photoDownloader.photos.chunked(into: 3).count){index in
+                        Text("Une row")
+                        GridRow{
+                            if(photoDownloader.photos.chunked(into: 3)[index].count>0){
+                                AsyncImage(url: URL(string: photoDownloader.photos.chunked(into: 3)[index][0].url)).frame(width: 10, height: 10).scaledToFit()
+                            }
+                            if(photoDownloader.photos.chunked(into: 3)[index].count>1){
+                                AsyncImage(url: URL(string: photoDownloader.photos.chunked(into: 3)[index][1].url)).frame(width: 10, height: 10).scaledToFit()
+                            }
+                            if(photoDownloader.photos.chunked(into: 3)[index].count>2){
+                                AsyncImage(url: URL(string: photoDownloader.photos.chunked(into: 3)[index][2].url)).frame(width: 10, height: 10).scaledToFit()
+                            }
+                        }.frame(width: 300)
                     }
                 }
+            }.onAppear{
+                photoDownloader.downloadPhotos(urlString: "https://jsonplaceholder.typicode.com/photos?albumId=\(self.gallery.id)")
             }
-        }.onAppear{
-            photoDownloader.downloadPhotos(urlString: "https://jsonplaceholder.typicode.com/photos?albumId=\(self.gallery.id)")
+        }
+    }
+}
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
         }
     }
 }
